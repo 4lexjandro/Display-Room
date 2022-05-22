e7106c47e56fa734d54ed6a206b116092bc703e1
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-05-22 18:20:06 -0400

    added routes and parts folders for display room, will be website where i showcase artwork soon to be

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './Routes/Home'
+import Contact from './Routes/Contact'
+import Notfound from './Routes/Notfound'
+import Shop from './Routes/Shop'
+import Bar from './Parts/Bar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    
+    
+   
+   <Router>
+   <Bar/>
+   
+     <Routes>
+     <Route path='/' element={<Home />} />
+     <Route path='/home' element={<Home />} />
+     <Route path='/shop' element={<Shop />} />
+     <Route path='/contact' element={<Contact />} />
+     <Route path='/notfound' element={<Notfound />} />
+     <Route path='/*' element={<Notfound />} />
+     </Routes>
+   </Router>
+   <div className="bottom"> 
+   {/* Footer will go here */}
+      </div>
+
+   
+   </>
   );
 }
 
